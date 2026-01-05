a33f0b1e323beda5a74c429c19325a14b52d8d01
unityport518-create <unityport518@gmail.com>
2026-01-05 13:53:47 +0200

    Add files via upload

---
+++ Tag1_Einstieg/Tag1_Einstieg/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hallo! Wie heißt du?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Wie alt bist du?");
+        string alter = Console.ReadLine();
+        Console.WriteLine("Wie ist dein Wohnort?");
+        string wohnort = Console.ReadLine();
+        Console.WriteLine("--------");
+        Console.WriteLine("Dein Name ist " + name+".");
+        Console.WriteLine($"Du bist {alter} Jahre alt und wohnst in {wohnort}!");
+        Console.WriteLine("Schön dich kennenzulernen {0} !", name);
+        Console.WriteLine("________");
+        Console.WriteLine("Drücke eine Taste zum Beenden!");
+        Console.ReadKey();
+    }
+}
