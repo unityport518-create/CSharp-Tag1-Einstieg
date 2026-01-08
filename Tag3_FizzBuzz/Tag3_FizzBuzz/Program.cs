d7db5aadb0018d4ae938e44fa7cd218fbd56f4dc
unityport518-create <unityport518@gmail.com>
2026-01-08 16:42:48 +0200

    Add files via upload

---
+++ Tag3_FizzBuzz/Tag3_FizzBuzz/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Programm
+{
+    static void Main()
+    {
+        int i = 1;
+        while (i != 101)
+        {
+            
+            
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+               
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+               
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+               
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+            i++;
+
+                
+            
+            
+        }
+        Console.WriteLine("Bitte drücken Sie eine beliebige Taste, um das Programm zu beenden.");
+        Console.ReadKey();
+    }
+}
